2f8d6b330be5372990feb65ab0dd3182f53789a3
Mariano Ferrisi <marianoferrisi23@gmail.com>
2022-06-04 12:33:07 -0300

    subido

---
+++ script.js
@@ -0,0 +1,15 @@
+const grande = document.querySelector('.grande')
+const punto = document.querySelectorAll('.punto')
+
+punto.forEach((cadapunto, i) => {
+    punto[i].addEventListener('click', () => {
+        let posicion = 1
+        let operacion = operacion * -50
+        grande.style.transform = 'translateX(${operacion}%)'
+        punto.forEach((cadapunto, i)=>{ 
+            punto[i].classList.remove('activo') })
+            punto[i].classList.add('activo')
+    })
+
+});
+
